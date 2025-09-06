81c9778e2153415fbd446e839e966719bceb5bac
viniciuscorreia8787 <viniciuscorreia8787@gmail.com>
2025-09-06 18:36:25 -0300

    Update app.js - check if has object type messages

--- app.js
+++ app.js
@@ -28,32 +28,80 @@ app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
-  res.write('Processing message...\n');
 
-  const waAccessToken = process.env.WA_ACCESS_TOKEN;
-  const url = `https://graph.facebook.com/v22.0/843764115476151/messages`;
-  const headers = {
-    'Authorization': `Bearer ${waAccessToken}`,
-    'Content-Type': 'application/json'
-  };
+  // Check if request body contains object named "messages"
+  const messagesObject = req.body && req.body.entry && req.body.entry[0] && req.body.entry[0].changes && req.body.entry[0].changes[0] && req.body.entry[0].changes[0].value && req.body.entry[0].changes[0].value.messages;
+  if (messagesObject) {
 
-  const data = {
-    'messaging_product': 'whatsapp',
-    'to': '5511993094820',
-    'type': 'template',
-    'template': {
-      'name': 'hello_world',
-      'language': {
-        'code': 'en_US'
+    //Example object structure:
+    //   {
+    //   "object": "whatsapp_business_account",
+    //   "entry": [
+    //     {
+    //       "id": "1882513740000000",
+    //       "changes": [
+    //         {
+    //           "value": {
+    //             "messaging_product": "whatsapp",
+    //             "metadata": {
+    //               "display_phone_number": "15550000000",
+    //               "phone_number_id": "843764110000000"
+    //             },
+    //             "contacts": [
+    //               {
+    //                 "profile": {
+    //                   "name": "Vinicius"
+    //                 },
+    //                 "wa_id": "5511999999999"
+    //               }
+    //             ],
+    //             "messages": [
+    //               {
+    //                 "from": "5511999999999",
+    //                 "id": "wamid.HBgNNTUxMTk5MzA5NDgyMBUCABIYFjNFQjAyMEU5M0E0RTQzRUM2NjJEMUQA",
+    //                 "timestamp": "1757193553",
+    //                 "text": {
+    //                   "body": "Teste sete =S"
+    //                 },
+    //                 "type": "text"
+    //               }
+    //             ]
+    //           },
+    //           "field": "messages"
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // }
+
+    res.write('Processing message...\n');
+
+    const waAccessToken = process.env.WA_ACCESS_TOKEN;
+    const url = `https://graph.facebook.com/v22.0/843764115476151/messages`;
+    const headers = {
+      'Authorization': `Bearer ${waAccessToken}`,
+      'Content-Type': 'application/json'
+    };
+
+    const data = {
+      'messaging_product': 'whatsapp',
+      'to': '5511993094820',
+      'type': 'template',
+      'template': {
+        'name': 'hello_world',
+        'language': {
+          'code': 'en_US'
+        }
       }
-    }
-  };
+    };
 
-  fetch(url, { method: 'POST', headers, body: JSON.stringify(data) })
-    .then(response => response.json())
-    .then(data => console.log(`Response received ${new Date().toISOString().replace('T', ' ').slice(0, 19)}`))
-    .catch(error => console.error(error));
+    fetch(url, { method: 'POST', headers, body: JSON.stringify(data) })
+      .then(response => response.json())
+      .then(data => console.log(`Response received ${new Date().toISOString().replace('T', ' ').slice(0, 19)}`))
+      .catch(error => console.error(error));
 
+    res.end('\n'.repeat(100) + '\n\n');  // corrected newline at the end
+  }
   res.status(200).end();
 });
 
